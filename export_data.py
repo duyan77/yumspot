36cfb78e8a10bf6810b8b3e29878a41b149cb106
duyan77 <anbui5948@gmail.com>
2025-05-22 10:23:10 +0700

    backup dữ liệu

--- export_data.py
+++ export_data.py
@@ -1,4 +1,12 @@
+import os
+
+import django
 from django.core.management import call_command
 
-with open("data.json", "w", encoding="utf-8") as f:
-    call_command("dumpdata", "store", indent=2, stdout=f)
+# Thiết lập môi trường Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yumspot.settings')
+django.setup()
+
+# Dump dữ liệu từ app "store" ra file UTF-8
+with open("backup_data.json", "w", encoding="utf-8") as f:
+	call_command("dumpdata", "store", indent=2, stdout=f)
